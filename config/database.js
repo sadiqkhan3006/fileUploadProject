f104be315aad2d955c634f787c0afdb45a9b4f8d
sadiqkhan3006 <sadiqkhan3006@gmail.com>
2023-09-13 15:10:43 +0530

    Setting up files & setting up our connections with cloudinary and MongodbAtlas

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+exports.dbconnect = async () => {
+  await mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("Db connected successfully"))
+    .catch((err) => {
+      console.log(err.message);
+      console.log("Error while connecting to db..");
+      process.exit(1);
+    });
+};
