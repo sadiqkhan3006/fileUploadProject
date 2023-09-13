283019ff9f32b1d0362e76852d0dc55662533dab
sadiqkhan3006 <sadiqkhan3006@gmail.com>
2023-09-13 23:51:51 +0530

    Updating business logic by adding extension to the path and testing using POSTMAN

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,16 +1,26 @@
 const file = require("../models/file");
 //local file upload ///
 
-exports.localFileUplaod = async (req, res) => {
+exports.localFileUpload = async (req, res) => {
   try {
     //fetch file //
     const file = req.files.file;
     console.log("File dekhlo guys: ", file);
-    let path = __dirname + "/files" + Date.now();
+    let pathArray = file.name.split(".");
+    let path =
+      __dirname +
+      "/files/" +
+      Date.now() +
+      `.${pathArray[pathArray.length - 1]}`;
     console.log("Path dekhlo : ", path);
     file.mv(path, (err) => {
-      console.log(err.message);
-      console.log("error while moving the file");
+      //console.log(err.message);
+      if (err) {
+        console.log("error while moving the file");
+        return res.status(500).json({ error: "File move failed" });
+      } else {
+        console.log("File moved sucessfully");
+      }
     });
     res.json({
       success: true,
@@ -18,5 +28,9 @@ exports.localFileUplaod = async (req, res) => {
     });
   } catch (error) {
     console.log(error.message);
+    res.json({
+      success: false,
+      message: "Error while upploading the file",
+    });
   }
 };
