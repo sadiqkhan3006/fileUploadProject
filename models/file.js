a24e8866ad90cb3eb91b2009bdf7aba0ea477875
sadiqkhan3006 <sadiqkhan3006@gmail.com>
2023-09-17 17:57:26 +0530

    added nodemailer functionality which emails the client after their data is posted on the db

--- models/file.js
+++ models/file.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 const fileSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -14,6 +16,36 @@ const fileSchema = new mongoose.Schema({
     type: String,
   },
 });
-
+//post middleware//
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("DOC: ", doc);
+    //transporter
+    console.log(
+      process.env.MAIL_HOST,
+      process.env.MAIL_USER,
+      process.env.MAIL_PASS
+    );
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      //read foresenic book of networking usmei smtp ka article hai
+      //aws ki SQS and SNS service padhlena
+    });
+    //send mail
+    let info = await transporter.sendMail({
+      from: `Codehelp- by sadiq `,
+      to: doc.email,
+      subject: "New file uploaded to cloudinary",
+      html: `<h2>Hello ji</h2><p>File uploaded </p>`,
+    });
+    console.log("info: ", info);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 const file = mongoose.model("File", fileSchema);
 module.exports = file;
